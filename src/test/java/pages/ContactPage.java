f6c8cbcc1ca1d584a2ac4fb3503c7ed4f9d62823
UAnduU <ungureanuandreic@gmail.com>
2025-08-27 03:15:37 +0300

    1.3: Modified code so that the file can be directly uploaded from GitHub

--- src/test/java/pages/ContactPage.java
+++ src/test/java/pages/ContactPage.java
@@ -4,6 +4,9 @@ import loggerUtility.LoggerUtility;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import java.io.*;
+import java.net.*;
+import java.nio.file.*;
 
 import java.io.File;
 
@@ -27,7 +30,9 @@ public class ContactPage extends BasePage{
     @FindBy(name = "upload_file")
     private WebElement contactUploadFile;
 
-    public void fillContactForm(String name, String email, String subject, String message, String uploadValue){
+
+
+    public void fillContactForm(String name, String email, String subject, String message, String uploadValue) throws MalformedURLException {
 
     elementHelper.fillElement(contactName, name);
     LoggerUtility.infoLog("The user fills the Name field");
@@ -37,10 +42,16 @@ public class ContactPage extends BasePage{
     LoggerUtility.infoLog("The user fills the Subject field");
     elementHelper.fillElement(contactMessage, message);
     LoggerUtility.infoLog("The user fills the Message field");
-    File file = new File(uploadValue);
-    elementHelper.fillElement(contactUploadFile, file.getAbsolutePath());
-
-
+        String fileURL = "https://github.com/UAnduU/AutomationExercise_FinalProject/blob/30e1c2711a479efef4f3245c34480811c77ce5cb/src/test/resources/TEST.png";
+        String localFilePath = System.getProperty("java.io.tmpdir") + "TEST.png";
+        try (InputStream in = new URL(fileURL).openStream()) {
+            Files.copy(in, Paths.get(localFilePath), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        File file = new File(localFilePath);
+        elementHelper.fillElement(contactUploadFile, file.getPath());
+        LoggerUtility.infoLog("The user uploads the file");
     }
 
 }
--- src/test/java/tests/ContactTEST.java
+++ src/test/java/tests/ContactTEST.java
@@ -5,17 +5,19 @@ import pages.ContactPage;
 import pages.MainPage;
 import sharedData.SharedData;
 
+import java.net.MalformedURLException;
+
 public class ContactTEST extends SharedData {
 
     @Test
 
-    public void testMethod(){
+    public void testMethod() throws MalformedURLException {
 
         String nameValue="John Doe";
         String emailValue="john.doe2@email.com";
         String subjectValue="TEST Subject";
         String messageValue="This is a test message.";
-        String uploadValue="C:\\Users\\Andu\\IdeaProjects\\Automation_Project\\src\\test\\resources\\TEST.png";
+        String uploadValue="https://github.com/UAnduU/AutomationExercise_FinalProject/blob/30e1c2711a479efef4f3245c34480811c77ce5cb/src/test/resources/TEST.png";
 
         MainPage mainPage=new MainPage(getDriver());
         mainPage.clickConsentButton();
